1932c2bd54306281c1e0cc64367e5dfe0e1466b6
sriastuti <sria7253@gmail.com>
2018-10-12 10:20:13 +0700

    new update

--- KaryawanTest.java
+++ KaryawanTest.java
@@ -1,24 +1,24 @@
 public class KaryawanTest{
 	public static void main(String[] args) {
-	System.out.println("Perushaan XYZ");
+	System.out.println("Perusahaan XYZ");
 	System.out.println("Jabatan Programmer");
 	Programmer p = new Programmer();
 	p.setNama("Rani");
 	p.setAlamat("Cirebon");
 	p.setGaji(70);
 
-	System.out.println("Perushaan XYZ");
+	System.out.println("Perusahaan XYZ");
 	System.out.println("Jabatan Project Manager");
-	ProjectManager pm = new Pm();
+	ProjectManager pm = new ProjectManager();
 	pm.setNama("Riana");
 	pm.setAlamat("Bandung");
-	pm.setGaji(80);
+	pm.setGaji(80,"Tambahan uang");
 
-	System.out.println("Perushaan XYZ");
+	System.out.println("Perusahaan XYZ");
 	System.out.println("Jabatan Desainer");
 	Desainer d = new Desainer();
 	d.setNama("Rini");
 	d.setAlamat("Sukabumi");
-	d.setGaji(60);
+	d.setGaji(60,"Tambahan uang");
 	}
 }
